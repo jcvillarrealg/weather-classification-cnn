7e65b21987b779d9ba82cce34048d0a5dd5e3b40
jcvillarrealg <joseca.villarreal@gmail.com>
2017-05-29 23:55:16 +0100

    Python Script + download of images
    Created a Python program that collects all the information from the
    images hosted in Flickr.com and ran it as a test. Images are downloaded
    as expected and information collected and written into txt file

---
+++ Dataset/Fog/ImageScraper.py
@@ -0,0 +1,92 @@
+
+# coding: utf-8
+
+# In[27]:
+
+from lxml import html
+import requests
+import urllib
+
+print("What is the name of the file that contains the urls?")
+urls_file = raw_input()
+
+urls = []
+
+with open(urls_file) as file_obj:
+	lines = file_obj.readlines()
+
+urls = [line.strip() for line in lines]
+
+count = 0
+
+pending_urls = []
+
+pending_file = open("pending_" + urls_file, "w")
+info_file = open("images_info.txt", "w")
+
+info_file.write("FileName,Title,,Author,License,Comments,URL")
+
+
+
+for url in urls:
+
+	if "https://www.flickr" not in url:
+		pending_file.write(url+"\n")
+		continue
+
+	count += 1
+
+	# In[2]:
+
+	page = requests.get(url)
+
+
+	# In[3]:
+
+	tree = html.fromstring(page.content)
+
+
+	# In[10]:
+
+	#Author
+	try:
+		author = tree.xpath("//div[@class='attribution-info']/a/text()")[0]
+	except:
+		title = "Not Provided"
+
+	# In[11]:
+
+	#Title
+	try:
+		title = tree.xpath("//head/meta[@property='og:title']/@content")[0]
+	except:
+		title = "Not Provided"
+
+	# In[16]:
+
+	#License
+	try:
+		license = tree.xpath("//div[@class='photo-license-info']/a/@href")[0]
+	except:
+		title = "Not Provided"
+
+	# In[29]:
+
+	#Image URL
+	image_url = tree.xpath("//head/meta[@property='og:image']/@content")[0]
+
+
+	# In[31]:
+
+	#Image Download
+	urllib.urlretrieve(image_url, str(count).rjust(4, '0') + ".jpg")
+
+	#Writing info of the image
+	#FileName,Title,,Author,License,Comments,URL
+	saux = "\n" + str(count).rjust(4, '0') + ".jpg" + "," + title + "," + author + "," + license + "No Changes" + "," + url
+
+	info_file.write(saux.encode("utf-8"))
+
+info_file.close()
+pending_file.close()
+
