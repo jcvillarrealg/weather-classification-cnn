2ede65e200a1ed84de13a24df98749ea06985485
jcvillarrealg <joseca.villarreal@gmail.com>
2017-06-01 17:27:30 +0100

    Created MATLAB function to draw superpixels on the images and added content to Literature Survey document

---
+++ Code/drawsuperpixels.m
@@ -0,0 +1,16 @@
+function [ bValue ] = drawsuperpixels( arrImagesNames, iNumPixels)
+%drawsuperpixels Draw Superpixels on each image in the specified range
+%   Detailed explanation goes here
+count = 1
+for i = arrImagesNames
+    A =imread(char(i));
+    [L,N] = superpixels(A,iNumPixels);
+    BW = boundarymask(L);
+    f = figure('visible', 'off')
+    imshow(imoverlay(A,BW,'cyan'),'InitialMagnification',67)
+    saveas(f,char(pad(string(count),4,'left','0')),'jpg')
+    count = count + 1
+end
+bValue = true
+end
+
