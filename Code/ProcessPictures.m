5192b44e5c84f3864c850fc8e627b597cf31139c
jcvillarrealg <joseca.villarreal@gmail.com>
2017-06-12 10:55:41 +0100

    Added the images that were pending from the Rainy category with superpixels

--- Code/ProcessPictures.m
+++ Code/ProcessPictures.m
@@ -1,14 +1,14 @@
 clear
 %Identify the images to process
-files = dir('../Dataset/Sunny/*.jpg')
+files = dir('../Dataset/Rainy/1100.jpg');
 %Variable with the directory where images came from
-origin_dir = string('../Dataset/Sunny/');
+origin_dir = string('../Dataset/Rainy/');
 %Directory where the processed images will be saved
-directory = string('../Dataset/Sunny_SP/');
+directory = string('../Dataset/Rainy_SP/');
 %Number of desired pixels to add to the images
 iNumPixels = 100;
 %counter to indicate the names of the images
-count = 1;
+count = 1100;
 for file = files'
     %Identifying the name of the current image
     aux = file.name;
