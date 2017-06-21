d8dceb7bf1586a0977111ad0a145569de7f86279
jcvillarrealg <joseca.villarreal@gmail.com>
2017-06-21 11:51:51 +0100

    Work on the main function for complete pipeline

--- Code/weatherclassification.m
+++ Code/weatherclassification.m
@@ -13,6 +13,8 @@
 
 
 function weatherclassification(models, categories, numsuperpixels, C, runid)
+
+addpath('../../../caffe/matlab/demo/');
 % Define parameters
 numsuperpixels = 50; %Number of superpixels for the images
 C = 10; %Classifier constant
@@ -34,20 +36,42 @@ directoriesforimages = [];
 for i = 1:1:numel(categories)
     directoryoriginaux = string(sprintf('../Dataset/%s/', categories(i)));
     directoryfinalaux = string(sprintf('../Dataset/%s_marked/', categories(i)));
-   directoriesforimages = [directoriesforimages directoryfinalaux];
+   directoriesforspimages = [directoriesforimages directoryfinalaux];
    origindirectories = [origindirectories directoryoriginaux];
 end
 
 % If the number of superpixels is above 0, then we most process the images
 if numsuperpixels > 0
     for i = 1:1:numel(categories)
-       processpictures( origindirectories(i), directoriesforimages(i), numsuperpixels)
+       processpictures( origindirectories(i), directoriesforspimages(i), numsuperpixels)
     end
 end
 
 
 %Extract features with selected model
-
+%Directory for extracted features -> ../extractedFeatures/
+featuresdirectory = '../extractedFeatures/';
+for i = 1:1:numel(models)
+    for j=1:1:numel(categories)
+        disp(fprintf('[LOG] Category %s \n', categories(j)))
+        %For each model and each of its categories, extract features and
+        %save them to a file
+        current_dir = string(features_base_dir) + string(sprintf('%s/%s/', models(i), categories(j)));
+        disp(current_dir)
+        [code, code_v, code_neg, code_v_neg] = matdeeprep(char(models(i)), 'ExtendedWeatherDatabase', char(categories(j)));
+        
+        save(char(current_dir + string('_positive_train_features.mat')), 'code');
+        save(char(current_dir + string('_positive_test_features.mat')), 'code_v');
+        save(char(current_dir + string('_negative_train_features.mat')), 'code_neg');
+        save(char(current_dir + string('_negative_test_features.mat')), 'code_v_neg');
+        
+        code = [];
+        code_v = [];
+        code_neg = [];
+        code_v_neg = [];
+        
+    end
+end
 
 
 %Train and Test Classifier with extracted features
